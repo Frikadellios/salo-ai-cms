9b17057399a5d281f73bcb651ee087d43e362b26
Hrihorii Ilin <devopsick@pm.me>
2024-06-17 18:54:08 +0000

    ++

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,28 @@
-import node from '@astrojs/node'
-import partytown from '@astrojs/partytown'
-import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
-import Vue from '@astrojs/vue'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
-import Inspect from 'vite-plugin-inspect'
+import node from '@astrojs/node';
+import partytown from '@astrojs/partytown';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import Vue from '@astrojs/vue';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
+import Inspect from 'vite-plugin-inspect';
+import mdx from '@astrojs/mdx';
 
-import mdx from '@astrojs/mdx'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://salo-ai-cms.vercel.app',
   vite: {
-    plugins: [
-      Inspect({
-        build: true,
-        outputDir: '.vite-inspect',
-      }),
-      tailwindcss(),
-    ],
+    plugins: [Inspect({
+      build: true,
+      outputDir: '.vite-inspect'
+    }), tailwindcss()]
   },
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  integrations: [
-    react(),
-    Vue({
-      jsx: true,
-      devtools: true,
-    }),
-    partytown(),
-    sitemap(),
-    mdx(),
-  ],
-})
+  adapter: vercel(),
+  integrations: [react(), Vue({
+    jsx: true,
+    devtools: true
+  }), partytown(), sitemap(), mdx()]
+});
